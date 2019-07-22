2cbad3dd72f50f6274e91345b78c431c082bda5a
zhouyujun <zhouyj@smartdot.com.cn>
2019-07-23 01:06:56 +0800

    常量定义

---
+++ jfcs/src/com/zyj/jfcs/constants/AppConst.java
@@ -0,0 +1,22 @@
+package com.zyj.jfcs.constants;
+/**
+ * APP常量定义
+ * @author 周昱君
+ *
+ */
+public class AppConst {
+	/**
+	 * 应用ID， 对应 plugin.xml 的 overview视图中的ID
+	 */
+	public static final String APPLICATION_ID = "jfcs";
+	
+	/**
+	 * 关于系统菜单项 ID
+	 */
+	public static final String ACTION_ABOUT_SYSTEM = "com.zyj.jfcs.actions.aboutSystem";
+	
+	/**
+	 * 关于系统菜单项 ID
+	 */
+	public static final String ACTION_LOG_OFF = "com.zyj.jfcs.actions.logOff";
+}
---
+++ jfcs/src/com/zyj/jfcs/constants/ImagePath.java
@@ -0,0 +1,20 @@
+package com.zyj.jfcs.constants;
+
+public class ImagePath {
+	/**
+	 * 数据备份按钮图标
+	 */
+	public static final String ACTION_DATA_BACK = "icons/actions/dataBack.png";
+	
+	/**
+	 * 关于系统
+	 */
+	public static final String ACTION_ABOUT_SYSTEM = "icons/actions/aboutSystem.ico";
+	
+	/**
+	 * 注销
+	 */
+	public static final String ACTION_LOG_OFF = "icons/actions/logOff.ico";
+	
+	
+}
